42326bf0cba2a8f7e05c84cc30a2899bc6987076
holden adamec <holden.adamec@gmail.com>
2019-02-21 17:09:10 -0800

    Added library stuff for easy use on other bots

---
+++ src/main/java/frc/team972/drive/DriveTrain.java
@@ -0,0 +1,55 @@
+package frc.team972.drive;
+
+import frc.team972.util.*;
+import frc.team972.util.Motor.MotorType;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class DriveTrain {
+	Motor[] motors, leftSide, rightSide;
+	
+	public DriveTrain(XMLReader reader) {
+		String motorIDs = reader.parseXML(RobotSettings.DRIVE_MOTOR_IDS);
+		MotorType motorType = MotorType.getMotorType(reader.parseXML(RobotSettings.DRIVE_MOTOR_TYPE));
+		int motorCount = Integer.parseInt(reader.parseXML(RobotSettings.DRIVE_MOTOR_COUNT));
+		
+		motors = new Motor[motorCount];
+		rightSide = new Motor[motorCount/2];
+		leftSide = new Motor[motorCount/2];
+		
+		// Initializes the array of motors
+		for (int i = 0; i < motorCount; i++) {
+			int motorID = Integer.parseInt(motorIDs.substring(i*2, i*2+1));			
+			motors[i] = new Motor(motorType, motorID);
+		}
+	
+		// Sets up the left and right sides
+		for (int i = 0; i < motorCount/2; i++) {
+			leftSide[i] = motors[i];
+		}
+		for (int i = 0; i < motorCount/2; i++) {
+			rightSide[i] = motors[i+motorCount/2];
+		}
+		
+	}
+	
+	/**
+	 * Only use if drive motor type is TALON
+	 */
+	public void logOutputCurrent() {
+		double currentSum = 0;
+		for (int i = 0; i < motors.length; i++) {
+			currentSum += motors[i].getOutputCurrent();
+		}
+		SmartDashboard.putNumber("avg drivetrain current draw", currentSum/motors.length);
+	}
+	
+	public void driveSides(double leftSideSpeed, double rightSideSpeed) {
+		for (Motor l: leftSide) {
+			l.set(leftSideSpeed);
+		}
+		for (Motor r: rightSide) {
+			r.set(-rightSideSpeed);
+		}
+	}
+
+}
---
+++ src/main/java/frc/team972/util/Motor.java
@@ -0,0 +1,73 @@
+package frc.team972.util;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.*;
+
+public class Motor {
+	WPI_TalonSRX talon;
+	Spark spark;
+	Victor victor;
+	MotorType type;
+	
+	public Motor(MotorType type, int ID) {
+		switch (type) {
+		case TALON:
+			talon = new WPI_TalonSRX(ID);
+			talon.set(ControlMode.PercentOutput, 0);
+			break;
+		case SPARK:
+			spark = new Spark(ID);
+			break;
+		case VICTOR:
+			victor = new Victor(ID);
+			break;
+		default:
+			System.out.println("ERROR! INVALID MOTOR TYPE!");
+			break;
+		}
+		this.type = type;
+	}
+	
+	public void set(double speed) {
+		switch (type) {
+		case TALON:
+			talon.set(speed);
+			break;
+		case SPARK:
+			spark.set(speed);
+			break;
+		case VICTOR:
+			victor.set(speed);
+			break;
+		}
+	}
+	
+	/**
+	 * This method only works on talons
+	 */
+	public double getOutputCurrent() {
+		if (type == MotorType.TALON)
+			return talon.getOutputCurrent();
+		else
+			return -1;
+	}
+	
+	public enum MotorType {
+		TALON, SPARK, VICTOR;
+		
+		public static MotorType getMotorType(String readerOutput) {
+			switch (readerOutput) {
+			case "TALON":
+				return MotorType.TALON;
+			case "SPARK":
+				return MotorType.SPARK;
+			case "VICTOR":
+				return MotorType.VICTOR;
+			default:
+				return null;
+			}
+		}
+	}
+
+}
